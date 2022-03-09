6045d41e85c7d2312f9d387ab983f24de61de513
eric <ppp458732@gmail.com>
2022-03-09 14:35:31 +0800

    First version of practice (no controller)

--- C4_1_TableVew/ViewController.swift
+++ C4_1_TableVew/ViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var names = ["1", "2", "3", "4", "5"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        cell.imageView?.image = UIImage(named: "facebook")
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
